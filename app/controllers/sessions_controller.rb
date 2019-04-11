561c0e5a28d2ed5d0a7884b4ad89e7178db42c7d
Carley Tripp <carley.tripp@outlook.com>
2019-04-11 22:16:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     if @user = User.find_or_create_by(id: auth['uid'])
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
     end
   end
 
